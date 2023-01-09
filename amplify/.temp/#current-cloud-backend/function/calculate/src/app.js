d062371ac94d9716ac6400d34a8fb981d92ee452
Stefan Millar <stefanmillar159@gmail.com>
2023-01-09 11:48:25 -0500

    more changes

---
+++ amplify/.temp/#current-cloud-backend/function/calculate/src/app.js
@@ -0,0 +1,116 @@
+/*
+Copyright 2017 - 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+    http://aws.amazon.com/apache2.0/
+or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+
+// declare a new express app
+const app = express()
+app.use(bodyParser.json())
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+// Enable CORS for all methods
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "*")
+  next()
+});
+
+const rpeCalculator = require('./scripts/rpeCalculator');
+
+/**********************
+ * Example get method *
+ **********************/
+
+app.get('/calculate', (req, res) => {
+	let weight = req.query.weight;
+	let reps = req.query.reps;
+	let rpe = req.query.rpe;
+
+	if(parseFloat(weight) == NaN) {
+		res.json({
+			error: 1,
+			msg: 'Please enter a valid weight.'
+		});
+
+		return;
+	}
+
+	if(weight < 0) {
+		res.json({
+			error: 1,
+			msg: 'Weight must be greater then 0.'
+		});
+
+		return;
+	}
+
+	let weightTable = rpeCalculator.calculateWeights(weight, reps, rpe);
+
+	res.json(weightTable);
+});
+
+app.get('/calculate/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'get call succeed!', url: req.url});
+});
+
+/****************************
+* Example post method *
+****************************/
+
+app.post('/calculate', function(req, res) {
+  // Add your code here
+  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+});
+
+app.post('/calculate/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'post call succeed!', url: req.url, body: req.body})
+});
+
+/****************************
+* Example put method *
+****************************/
+
+app.put('/calculate', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
+
+app.put('/calculate/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+});
+
+/****************************
+* Example delete method *
+****************************/
+
+app.delete('/calculate', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+app.delete('/calculate/*', function(req, res) {
+  // Add your code here
+  res.json({success: 'delete call succeed!', url: req.url});
+});
+
+app.listen(3000, function() {
+    console.log("App started")
+});
+
+// Export the app object. When executing the application local this does nothing. However,
+// to port it to AWS Lambda we will create a wrapper around that will load the app from
+// this file
+module.exports = app
